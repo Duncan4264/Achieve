9ca4be3eec6f589cd538c6f0f2e5b3049367107b
Cyrus Duncan <cyrusd4264@gmail.com>
2020-04-20 16:55:46 -0600

    Update confirmUnsuspend.blade.php

--- resources/views/confirmUnsuspend.blade.php
+++ resources/views/confirmUnsuspend.blade.php
@@ -3,11 +3,11 @@
 
 @section('content')
 <div>
-<h1>Would you like to Suspend? {{$id}}</h1>
+<h1>Would you like to UnSuspend? {{$id}}</h1>
 <form action="processUnsuspend" method="post">
 <input type="hidden" name = "_token" value = "{{csrf_token()}}" />
 <input type="hidden" name = "id" value = "{{$id}}" />
-<button type="submit" formaction="processUnsuspend">Suspend</button>
+<button type="submit" formaction="processUnsuspend">UnSuspend</button>
 <button type="button" formaction="admin">Cancel</button>
 </form>
 
